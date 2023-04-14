487bd310cff76c94feccce2a801ffd239e755f62
Liam Duffy <liamdanielduffy@gmail.com>
2023-04-14 15:37:08 -0400

    zen mode 2

--- src/pages/feed2.tsx
+++ src/pages/feed2.tsx
@@ -1,7 +1,55 @@
+import { bsky } from "@/services/atp";
+import { AppBskyFeedDefs } from "@atproto/api";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
 export default function Feed2() {
-    return (
-      <div className="flex min-h-full flex-col justify-center items-center py-12 sm:px-6 lg:px-8 bg-forest">
-        <div className="m-4 bg-yellow-100 w-64 h-64 rounded"></div>
-      </div>
-    );
+  const [currentPoastIndex, setCurrentPoastIndex] = useState(0);
+
+  const [poasts, setPoasts] = useState<AppBskyFeedDefs.FeedViewPost[]>([]);
+
+  const currentPoast = poasts[currentPoastIndex];
+
+  useEffect(() => {
+    bsky.feed
+      .getTimeline({
+        limit: 30,
+      })
+      .then((resp) => setPoasts(resp.data.feed));
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentPoastIndex === poasts.length - 1) {
+        return setCurrentPoastIndex(0);
+      } else {
+        return setCurrentPoastIndex(currentPoastIndex + 1);
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [currentPoastIndex, poasts]);
+
+  return (
+    <div className="flex min-h-full flex-col justify-center items-center py-12 sm:px-6 lg:px-8 bg-forest">
+
+
+
+      <motion.div
+        key={currentPoast?.post.cid}
+        // transition={{ duration: 1, ease: "easeIn" }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="text-center w-full max-w-sm bg-gradient-to-r from-purple-500 to-pink-500 p-8 rounded-t text-lg text-gray-200"
+      >
+        {currentPoast?.post.record["text"]}
+      </motion.div>
+      {/* <div className="bg-orange-300 w-full max-w-sm rounded-b p-4 flex flex-col">
+          <button className="text-lg bg-orange-100 p-2 rounded">
+            Next Poast
+          </button>
+        </div> */}
+    </div>
+  );
 }
