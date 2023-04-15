b9dbd1cfbfba9c929c977e85d765d1feb8ac0596
Liam Duffy <liamdanielduffy@gmail.com>
2023-04-15 13:15:29 -0400

    fix broken hook reference

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,9 @@
-import { useBackground } from "@/state/slices/theme"
+import { useBackgroundImage } from "@/state/slices/theme"
 
 export const Background: React.FC<{ children: React.ReactNode, className?: string }> = ({ children, className }) => (
     <div
         className={className}
-        style={{ backgroundImage: `url("${useBackground()}")` }}
+        style={{ backgroundImage: `url("${useBackgroundImage()}")` }}
     >
         {children}  
     </div>
